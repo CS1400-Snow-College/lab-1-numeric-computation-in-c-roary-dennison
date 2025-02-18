cec0a807f720c444d7bdd6b77ae71d40090b39f6
student <student@ENGR2JQD5M3.ad.snow.edu>
2025-02-18 13:54:22 -0700

    finished step: 5

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿
-double a = 19;
-double b = 23;
-double c = 8;
-double d = (a + b) / c;
-Console.WriteLine(d);
-double max = double.MaxValue;
-double min = double.MinValue;
-Console.WriteLine($"The range of double is {min} to {max}");
-double third = 1.0 / 3.0;
-Console.WriteLine(third);
+﻿decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
