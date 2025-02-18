e9ea9c88108deab80774d2a26bf637918908e8af
student <student@ENGR2JQD5M3.ad.snow.edu>
2025-02-18 13:53:02 -0700

    finished step: 4

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+﻿
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
